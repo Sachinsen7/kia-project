daa96981d39cb86d714064e405c62fd4d0718285
Sachin Sen <sachinsen1920@gmail.com>
2025-09-09 09:54:34 +0530

    update cityscene

--- src/components/CityScene.tsx
+++ src/components/CityScene.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+type CitySceneProps = {
+  onSelect: (id: string) => void;
+};
 
-function CityScene() {
-  return <div>CityScene</div>;
+export default function CityScene({ onSelect }: CitySceneProps) {
+  return (
+    <div
+      className="w-full h-full bg-blue-200 flex items-center justify-center"
+      onClick={() => onSelect("building1")}
+    >
+      <p>3D City Scene (click to select building1)</p>
+    </div>
+  );
 }
-
-export default CityScene;
