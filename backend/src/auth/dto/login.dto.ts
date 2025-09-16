5663c06ab3f6ee54983247e4ae14efe64533a130
Ishantp01 <163533317+Ishantp01@users.noreply.github.com>
2025-09-16 13:54:51 +0530

    updated the dto of user signup and user login

--- backend/src/auth/dto/login.dto.ts
+++ backend/src/auth/dto/login.dto.ts
@@ -1,10 +1,11 @@
-import { IsString } from "class-validator";
+import { IsEmail, IsString, MinLength } from "class-validator";
 
 
 export class LoginDto{
-    @IsString()
+    @IsEmail()
     userName: string;
 
     @IsString()
+    @MinLength(8)
     password: string;
 }
