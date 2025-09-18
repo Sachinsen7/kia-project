c4f50e27fa3e5dcb0fa7da754da0bdfbd265f3b4
Ishantp01 <163533317+Ishantp01@users.noreply.github.com>
2025-09-18 15:49:25 +0530

    made the controller to toggle likes

--- kia-backend/modules/qna/qna.controller.js
+++ kia-backend/modules/qna/qna.controller.js
@@ -29,3 +29,25 @@ exports.getAllQna = async (req, res) => {
         res.status(500).json({message: "Server error"});
     }
 }
+
+exports.toggleLike = async (req, res) => {
+    try{
+        const qna = await Qna.findById(req.params.id);
+        if(!qna) return res.status(400).json({message: "Qna not found"});
+
+        const userId = req.user.id;
+        const isLiked = qna.likes.includes(userId);
+
+        if(isLiked){
+            qna.likes = qna.likes.filter(id => id.toString() !== userId);
+        }else{
+            qna.likes.push(userId);
+        }
+
+        await qna.save();
+        res.json({message: isLiked? "unliked" : "liked" , qna});
+    }
+    catch(err){
+        res.status(500).json({message: "Server error"});
+    }
+}
