e30fd2794561a36ee58577d7275ee03413dc0b04
Ishantp01 <163533317+Ishantp01@users.noreply.github.com>
2025-09-18 16:07:09 +0530

    created the middlewares to make the route protected

--- kia-backend/modules/qna/qna.routes.js
+++ kia-backend/modules/qna/qna.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router;
+const router = express.Router();
 const {createQna, deleteQna, getAllQna, toggleLike} = require("./qna.controller");
 
 
