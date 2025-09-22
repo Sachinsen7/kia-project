887637a38518969119ba23b7ec0f0e6e903ceed3
Ishantp01 <163533317+Ishantp01@users.noreply.github.com>
2025-09-22 15:08:58 +0530

    updates usermodel

--- kia-backend/modules/users/user.model.js
+++ kia-backend/modules/users/user.model.js
@@ -3,7 +3,7 @@ const mongoose = require ("mongoose");
 const userSchema = new mongoose.Schema({
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
-    title: {type: string, required: true},
+    title: {type: String, required: true},
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     region: { type: String, required: true },
