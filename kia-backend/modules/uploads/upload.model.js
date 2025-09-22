8e489fda3c3e891426382f3d6eeee9ad72612303
sapeksh <sapekshvishwakarma@gmail.com>
2025-09-22 14:13:26 +0530

    fix upload

--- kia-backend/modules/uploads/upload.model.js
+++ kia-backend/modules/uploads/upload.model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const uploadSchema = new mongoose.Schema({
   filename: { type: String, required: true },
   originalName: { type: String, required: true },
-  fileType: { type: String, required: true }, // e.g. "image/png", "video/mp4"
+  fileType: { type: String, required: true },
   category: { type: String, enum: ["best-practice", "greeting-video"], required: true },
   uploadedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   path: { type: String, required: true }
--- kia-backend/modules/uploads/upload.routes.js
+++ kia-backend/modules/uploads/upload.routes.js
@@ -4,10 +4,10 @@ const { uploadFile, getAllUploads, downloadFile, deleteFile } = require("./uploa
 const authMiddleware = require("../../middleware/authMiddleware");
 const upload = require("./upload.middleware");
 
-// Upload routes
+
 router.post("/", authMiddleware, upload.single("file"), uploadFile);
 router.get("/", authMiddleware, getAllUploads);
 router.get("/:id/download", authMiddleware, downloadFile);
-router.delete("/:id", authMiddleware, deleteFile); // ideally admin only
+router.delete("/:id", authMiddleware, deleteFile); 
 
 module.exports = router;
