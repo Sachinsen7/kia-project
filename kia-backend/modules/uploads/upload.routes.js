afc3324ee6f87dbbb994353803cf7152b0672063
Sachin Sen <sachinsen1920@gmail.com>
2025-09-23 16:52:43 +0530

    fix

--- kia-backend/modules/uploads/upload.routes.js
+++ kia-backend/modules/uploads/upload.routes.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { uploadFile, getAllUploads, downloadFile, deleteFile } = require("./upload.controller");
+const {
+  uploadFile,
+  getAllUploads,
+  downloadFile,
+  deleteFile,
+} = require("./upload.controller");
 const authMiddleware = require("../../middleware/authMiddleware");
 const upload = require("./upload.middleware");
-
 
 router.post("/", authMiddleware, upload.single("file"), uploadFile);
 router.get("/", authMiddleware, getAllUploads);
 router.get("/:id/download", authMiddleware, downloadFile);
-router.delete("/:id", authMiddleware, deleteFile); 
+router.delete("/:id", authMiddleware, deleteFile);
 
 module.exports = router;
