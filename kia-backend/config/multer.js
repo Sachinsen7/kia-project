71bc84a3d75969648fd2c98782c81f3e3bbb467a
Ishantp01 <163533317+Ishantp01@users.noreply.github.com>
2025-09-24 17:08:16 +0530

    makes dir  uploads in backend

--- kia-backend/config/multer.js
+++ kia-backend/config/multer.js
@@ -1,13 +1,23 @@
-const multer = require('multer');
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+
+const uploadDir = path.join(__dirname, "..", "uploads");
+
+// ✅ Ensure uploads folder exists
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
-    destination:  (req, file, cb) => {
-        cb(null, 'uploads/')
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now()+"-"+file.originalname);
-    },
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
 });
 
-const upload = multer({ storage })
+const upload = multer({ storage });
+
 module.exports = upload;
