fc96066bff810ba2c947893b8b8645f7c830eab3
Sachin Sen <sachinsen1920@gmail.com>
2025-09-24 18:17:55 +0530

    uupdated links placemnnet

--- client/src/app/data/Links.tsx
+++ client/src/app/data/Links.tsx
@@ -19,9 +19,11 @@ export type CityLink = {
 
 export const links: CityLink[] = [
   { id: "live-link", name: "Live Event", x: 57, y: 20, icon: Cast },
-  { id: "welcome", name: "Welcome to 2025 GOEF", x: 15, y: 35, icon: Home },
-  { id: "about", name: "About 2025 GOEF", x: 80, y: 10, icon: Info },
+  { id: "welcome", name: "Welcome to 2025 GOEF", x: 80, y: 10, icon: Home },
+  { id: "about", name: "About 2025 GOEF", x: 15, y: 35, icon: Info },
   { id: "history-goef", name: "History of GOEF", x: 30, y: 52, icon: Archive },
+
+  //
   {
     id: "ask-kia",
     name: "Questions that shape our future",
