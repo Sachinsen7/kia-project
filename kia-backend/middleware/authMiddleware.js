f17c471c1baa25bb674b9468c33235a90666e970
Ishantp01 <163533317+Ishantp01@users.noreply.github.com>
2025-09-24 14:12:43 +0530

    reconfigures the auth middleware

--- kia-backend/middleware/authMiddleware.js
+++ kia-backend/middleware/authMiddleware.js
@@ -1,28 +1,25 @@
 const jwt = require("jsonwebtoken");
 
-const authMiddleware = (req, res, next) => {
+const authMiddleware = (erq, res, next) => {
   try {
     const authHeader = req.headers.authorization;
 
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      return res.status(401).json({ message: "No token provided" });
+    if(!authHeader || !authHeader.startsWith("Bearer ")){
+      return res.status(401).json({success: false, message: "Unauthorized"})
     }
 
     const token = authHeader.split(" ")[1];
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-      if (err) {
-        return res.status(401).json({ message: "Invalid or expired token" });
+      if(err) {
+        return res.status(401).json({success: false, message: "Invalid or  expored token"});
       }
 
-      req.user = decoded; // now you can access req.user.id, req.user.email
+      req.user = decoded;
       next();
-    });
-  } catch (err) {
-    res
-      .status(500)
-      .json({ message: "Auth middleware error", error: err.message });
+    })
   }
-};
-
-module.exports = authMiddleware;
+  catch(err) {
+    res.status(500).json({success: false, message: "Auth middleware error", error: err.message});
+  }
+}
