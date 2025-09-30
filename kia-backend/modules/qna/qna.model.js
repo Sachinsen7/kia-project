d85fbd977f06278c7fc4b42b5bdbabc7d7259af3
Sachin Sen <sachinsen1920@gmail.com>
2025-09-30 15:05:00 +0530

    remove country

--- kia-backend/modules/qna/qna.model.js
+++ kia-backend/modules/qna/qna.model.js
@@ -4,7 +4,7 @@ const qnaSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
     description: { type: String, required: true },
-    country: { type: String, required: true },
+    // country: { type: String, required: true },
     type: {
       type: String,
       required: true,
