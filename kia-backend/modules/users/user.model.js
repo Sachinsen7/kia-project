3c40323ae2a49a7b0724767ee359531de9862eaa
sparsh-15 <sparshsahu8435@gmail.com>
2025-09-30 18:18:11 +0530

    update user model for setpassword

--- kia-backend/modules/users/user.model.js
+++ kia-backend/modules/users/user.model.js
@@ -2,7 +2,7 @@ const mongoose = require ("mongoose");
 
 const userSchema = new mongoose.Schema({
     email: { type: String, unique: true, required: true },
-    password: { type: String, required: true },
+    password: { type: String },
     title: {type: String, required: true},
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
