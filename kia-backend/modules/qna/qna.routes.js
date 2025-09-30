18b8af643fc8f2de5773bf28d751f9b89cd59756
Sachin Sen <sachinsen1920@gmail.com>
2025-09-30 14:18:13 +0530

    fix add authMiddleware

--- kia-backend/modules/qna/qna.routes.js
+++ kia-backend/modules/qna/qna.routes.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const {createQna, deleteQna, getAllQna, toggleLike} = require("./qna.controller");
-const authMiddlewares = require("../../middleware/authMiddleware")
+const {
+  createQna,
+  deleteQna,
+  getAllQna,
+  toggleLike,
+} = require("./qna.controller");
+const authMiddlewares = require("../../middleware/authMiddleware");
 
-
-router.post("/", createQna);
-router.get("/", authMiddlewares,getAllQna);
+router.post("/", authMiddlewares, createQna);
+router.get("/", authMiddlewares, getAllQna);
 router.put("/:id/like", authMiddlewares, toggleLike);
-router.delete("/:id" , authMiddlewares, deleteQna);
+router.delete("/:id", authMiddlewares, deleteQna);
 
 module.exports = router;
