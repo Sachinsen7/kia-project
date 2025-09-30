ed697725d519fe0dc7d63be8571110b147985ab7
sparsh-15 <sparshsahu8435@gmail.com>
2025-09-30 18:17:59 +0530

    seed user added

---
+++ kia-backend/scripts/seedUsers.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const User = require("../modules/users/user.model");
+require("dotenv").config();
+
+
+const users = [
+  {
+    email: "sparshsahu8435@gmail.com",
+    title: "Mr.",
+    firstName: "John",
+    lastName: "Doe",
+    region: "Asia",
+    country: "India",
+    nationality: "Indian",
+    isActive: false,
+    password: "", // No password yet
+  },
+  {
+    email: "sparshsahu004@gmail.com",
+    title: "Mr.",
+    firstName: "lam",
+    lastName: "Doe",
+    region: "Asia",
+    country: "India",
+    nationality: "Indian",
+    isActive: false,
+    password: "", // No password yet
+  },
+  {
+    email: "albusseverus816@gmail.com",
+    title: "Mr.",
+    firstName: "albus",
+    lastName: "Doe",
+    region: "Asia",
+    country: "India",
+    nationality: "Indian",
+    isActive: false,
+    password: "", // No password yet
+  },
+  // ...add more users
+];
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(async () => {
+    await User.deleteMany({});
+    await User.insertMany(users);
+    console.log("Users seeded!");
+    process.exit();
+  });
