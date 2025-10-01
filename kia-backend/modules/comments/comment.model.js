058cfbf4261bd5048420f4ccd9c830d8bd3b047b
sparsh-15 <sparshsahu8435@gmail.com>
2025-10-01 16:02:07 +0530

    update comment model

--- kia-backend/modules/comments/comment.model.js
+++ kia-backend/modules/comments/comment.model.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const commentSchema = new mongoose.Schema({
   text: { type: String, required: true },
   qna: { type: mongoose.Schema.Types.ObjectId, ref: "Qna", required: true },
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  createdByName: { type: String, default: "Unknown" }
 }, { timestamps: true });
 
 module.exports = mongoose.model("Comment", commentSchema);
