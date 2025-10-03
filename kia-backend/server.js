c98f3a3b6660cd36cef48474a9a86368b5b16c3a
sparsh-15 <sparshsahu8435@gmail.com>
2025-10-03 13:54:28 +0530

    create cookies for sessions

--- kia-backend/server.js
+++ kia-backend/server.js
@@ -4,10 +4,20 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 const routes = require("./routes/index");
 const seedAdmin = require("./config/seedAdmin");
-const visitRouter = require("./modules/websiteVisit/visit.routes"); // make sure the path is correct
-
+const visitRouter = require("./modules/websiteVisit/visit.routes"); 
+const cookieParser = require("cookie-parser");
+const cookieTracker = require("./middleware/cookieTracker");
 
 const app = express();
+app.use(cookieParser());
+
+
+// Track visits for all requests
+app.use(cookieTracker);
+
+
+
+
 
 const allowedOrigins = [
   "https://kia-project-eight.vercel.app",
@@ -26,6 +36,7 @@ app.use(
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use("/api", routes);
 app.use("/api/visit", visitRouter);
