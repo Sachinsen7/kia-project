b2509ae80e50d4b3aad8f1edb1fa42ee639ddf42
Ishantp01 <163533317+Ishantp01@users.noreply.github.com>
2025-10-14 13:15:16 +0530

    fix(Comments): Add authentication middleware to getComments route
    - Integrate authMiddleware into the getComments route to ensure only authenticated users can access comments.
    - Maintain existing functionality for adding and deleting comments with authentication checks.
    - Enhance security and user access control for comment retrieval.

--- kia-backend/modules/comments/comment.routes.js
+++ kia-backend/modules/comments/comment.routes.js
@@ -4,7 +4,7 @@ const { addComment, getComments, deleteComment } = require("./comment.controller
 const authMiddleware = require("../../middleware/authMiddleware");
 
 router.post("/:qnaId", authMiddleware, addComment);  
-router.get("/:qnaId", getComments);                 
+router.get("/:qnaId",authMiddleware, getComments);                 
 router.delete("/:id", authMiddleware, deleteComment); 
 
 module.exports = router;
